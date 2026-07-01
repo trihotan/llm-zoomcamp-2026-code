55d5bc73c2c899c8ec908ec78954c7c6a4b77e28
trihotan <35389169+trihotan@users.noreply.github.com>
2026-07-01 05:11:34 +0000

    modified:   .gitignore 	new file:   Chapter 2/PGVector_vector_search.ipynb 	new file:   Chapter 2/sqlite_vector_search.ipynb 	new file:   Chapter 2/vector_search.ipynb 	modified:   __pycache__/ingest.cpython-312.pyc 	modified:   __pycache__/rag_helper.cpython-312.pyc 	new file:   homework/llm-zoomcamp-hw2/.python-version 	new file:   homework/llm-zoomcamp-hw2/README.md 	new file:   homework/llm-zoomcamp-hw2/__pycache__/embedder.cpython-312.pyc 	new file:   homework/llm-zoomcamp-hw2/download.py 	new file:   homework/llm-zoomcamp-hw2/embedder.py 	new file:   homework/llm-zoomcamp-hw2/homework_2_vector_search.ipynb 	new file:   homework/llm-zoomcamp-hw2/main.py 	new file:   homework/llm-zoomcamp-hw2/pyproject.toml 	new file:   homework/llm-zoomcamp-hw2/uv.lock 	renamed:    Chapter 1/ingest.py -> ingest.py 	renamed:    Chapter 1/rag_helper.py -> rag_helper.py

--- rag_helper.py
+++ rag_helper.py
@@ -1,3 +1,6 @@
+from minsearch import vector
+
+
 INSTRUCTIONS = """
 Your task is to answer questions from the course participants
 based on the provided context.
@@ -80,3 +83,44 @@ class RAGBase:
         prompt = self.build_prompt(query, search_results)
         answer = self.llm(prompt)
         return answer
+
+class RAGVector(RAGBase):
+    def __init__(self, embedder, **kwargs):
+        super().__init__(**kwargs)
+        self.embedder = embedder
+
+    def search(self, query, num_results = 5):
+        query_vector = self.embedder.encode(query)
+        filter_dict = {"course": self.course}
+        
+        return self.index.search(query_vector, num_results=num_results, filter_dict = filter_dict)
+
+class RAGPgVector(RAGBase):
+
+    def __init__(self, embedder, conn, **kwargs):
+        super().__init__(index=None, **kwargs)
+        self.embedder = embedder
+        self.conn = conn
+
+    def vec_to_str(vector):
+        return "[" + ",".join(str(x) for x in vector) + "]"
+
+    def search(self, query, num_results=5):
+        query_vector = self.embedder.encode(query)
+        query_str = vec_to_str(query_vector)
+
+        rows = self.conn.execute(
+            """
+            SELECT course, section, question, answer
+            FROM documents
+            WHERE course = %s
+            ORDER BY embedding <=> %s::vector
+            LIMIT %s
+            """,
+            (self.course, query_str, num_results)
+        ).fetchall()
+
+        return [
+            {"course": r[0], "section": r[1], "question": r[2], "answer": r[3]}
+            for r in rows
+        ]
